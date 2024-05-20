ca7b4f2230838becb7fefe6e600f5e4044134463
deedevo <idia6k@gmail.com>
2024-05-20 15:30:58 +0500

    Initial Commit

---
+++ Taskly/TasklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasklyApp.swift
+//  Taskly
+//
+//  Created by Dias Ibragim on 20.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TasklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
